dda745cb3a60754f97824453e143aa4b0f25a90f
adrgarcha <adrgarcha@alum.us.es>
2024-08-20 16:53:36 +0200

    fix theme hook

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,22 +1,13 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
-import { useColorScheme } from 'react-native';
-
 import { Colors } from '@/constants/Colors';
+import { useColorScheme } from './useColorScheme';
 
-export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+export function useThemeColor(props: { light?: string; dark?: string }, colorName: keyof typeof Colors.light & keyof typeof Colors.dark) {
+   const { colorScheme } = useColorScheme() ?? 'light';
+   const colorFromProps = props[colorScheme];
 
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
-  }
+   if (colorFromProps) {
+      return colorFromProps;
+   } else {
+      return Colors[colorScheme][colorName];
+   }
 }
