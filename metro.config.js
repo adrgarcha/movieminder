24b06aaae19cbe7ace1f4ddec2a2d797334ce101
adrgarcha <adrgarcha@alum.us.es>
2024-08-30 14:29:20 +0200

    fix eslint error

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
 
-const __dirname = path.resolve(path.dirname(''));
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname)
 
 module.exports = withNativeWind(config, { input: './global.css' })
